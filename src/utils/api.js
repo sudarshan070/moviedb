69056c6eb296a2f30373f8b0f77121b0ae998710
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-11-30 15:37:54 +0530

    api error fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export const baseURL = `https://api.themoviedb.org/3`
 
-export const API_KEY = process.env.REACT_APP_MDB_API;
+export const API_KEY = `31c4cb4b95c2c4836e6fb6d1f24f2ab7`;
 
 export const URL_YOUTUBE = 'https://www.youtube.com/embed/';
